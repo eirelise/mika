fb0353e598774cbf9fdff20d7976816c9d4a5e4a
Your Name <eir.elise@outlook.com>
2025-10-28 12:46:36 +0100

    hm

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 
 
 console.log("Hello, World!");
+console.log("This is Mika's index.js file.");
 
-let antall = 5;
-let ganger = 10;
-console.log(sum);
-const tekst = "This is a string";
-let sum = tekst + (antall * ganger);
+
+
+document.getElementById("my-h1").textContent = "Mika";
+
+let x=10+5;
+console.log(x);
+
+let age=25;
+let price=19.99;
+console.log("age");
